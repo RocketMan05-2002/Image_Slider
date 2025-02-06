6d4aecf23b5144a3db9be32502b93ed570d4402d
Rocketman <abhinayshambharkar1@gmail.com>
2025-02-06 19:39:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Slider from './components/ImageSlider/Slider'
+
+const App = () => {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+export default App

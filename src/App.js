8cdab00b13f72830e6b4a76649c92c2c0b0ca47a
Rocketman <abhinayshambharkar1@gmail.com>
2025-02-06 20:33:28 +0530

    add a note

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import NoFetchSlider from './components/NoFetchImageSlider/NoFetchImageSlider'
 const App = () => {
   return (
     <div>
+      {/* choose any one of following sliders
+      1. slider fetches images from picsum and creates a slider
+      2. Nofetchslider takes locally stored images and creates a slider
+      */}
       {/* <Slider /> */}
       <NoFetchSlider />
     </div>

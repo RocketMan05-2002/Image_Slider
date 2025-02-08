0ba5352744c23e79e50c64ece09020ca675c0908
Rocketman <abhinayshambharkar1@gmail.com>
2025-02-08 20:14:08 +0530

    added circle indicators

--- src/components/NoFetchImageSlider/NoFetchImageSlider.js
+++ src/components/NoFetchImageSlider/NoFetchImageSlider.js
@@ -1,48 +1,61 @@
-import { useState } from 'react';
-import planetImages from './Images';
-import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs';
-import './NoFetchImageSlider.css'
+import { useState } from "react";
+import planetImages from "./Images";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import "./NoFetchImageSlider.css";
 
 export default function NoFetchSlider() {
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-    const[currentSlide,setCurrentSlide] = useState(0);
-
-    function handlePrevious(){
-        setCurrentSlide(
-            currentSlide === 0 ? planetImages.length-1 : currentSlide-1
-        )
-    }
-
-    function handleNext(){
-        setCurrentSlide(
-            currentSlide === planetImages.length-1 ? 0 : currentSlide+1
-        )
-    }
-
-    return (
-        <div className='main-container'>
-            <div className='container'>
-                <BsArrowLeftCircleFill 
-                className='arrow arrow-left'
-                onClick={handlePrevious}
-                />
-                {planetImages.map((val, index) => (
-                    <img 
-                    key={index} 
-                    src={val} 
-                    alt={`planet-${index}`}
-                    className={
-                        currentSlide === index 
-                        ? "current-image"
-                        : "current-image hide-current-image"
-                    }
-                    />
-                ))}
-                <BsArrowRightCircleFill 
-                className='arrow arrow-right' 
-                onClick={handleNext}
-                />
-            </div>
-        </div>
+  function handlePrevious() {
+    setCurrentSlide(
+      currentSlide === 0 ? planetImages.length - 1 : currentSlide - 1
     );
+  }
+
+  function handleNext() {
+    setCurrentSlide(
+      currentSlide === planetImages.length - 1 ? 0 : currentSlide + 1
+    );
+  }
+
+  return (
+    <div className="main-container">
+      <div className="container">
+        <BsArrowLeftCircleFill
+          className="arrow arrow-left"
+          onClick={handlePrevious}
+        />
+        {planetImages.map((val, index) => (
+          <img
+            key={index}
+            src={val}
+            alt={`planet-${index}`}
+            className={
+              currentSlide === index
+                ? "current-image"
+                : "current-image hide-current-image"
+            }
+          />
+        ))}
+        <BsArrowRightCircleFill
+          className="arrow arrow-right"
+          onClick={handleNext}
+        />
+        <span className="circle-indicators">
+          {planetImages && planetImages.length
+            ? planetImages.map((_, index) => (
+                <button
+                  className={
+                    currentSlide === index
+                      ? "current-indicator"
+                      : "current-indicator inactive-indicator"
+                  }
+                  onClick={() => setCurrentSlide(index)}
+                ></button>
+              ))
+            : null}
+        </span>
+      </div>
+    </div>
+  );
 }
